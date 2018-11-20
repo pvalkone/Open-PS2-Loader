57b8fa7079e9d78dd30eba015474349150c6bdae
Petteri Valkonen <petteri.valkonen@iki.fi>
2018-11-20 17:59:42 +0200

    VMC (7135991) and GSM (60b8222) have been merged into the main build

--- build-elf.sh
+++ build-elf.sh
@@ -4,7 +4,7 @@ PS2SDK_REVISION=05bb1b6
 
 rm OPNPS2LD*.ELF
 docker build --tag pvalkone/opl:latest --build-arg PS2SDK_REVISION=${PS2SDK_REVISION} .
-docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c "git add --all && VMC=1 GSM=1 HIRES=1 make clean rebuild"
-OPLVERSION="$(docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c 'git add --all && make oplversion')-VMC-GSM-HIRES"
+docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c "git add --all && HIRES=1 make clean rebuild"
+OPLVERSION="$(docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c 'git add --all && make oplversion')-HIRES"
 mv OPNPS2LD.ELF "./OPNPS2LD-${OPLVERSION}.ELF"
 git reset
