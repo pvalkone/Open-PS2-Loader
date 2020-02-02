d56bfc98b2da0ba35cfa283a1df77faedc4cccf7
Petteri Valkonen <petteri.valkonen@iki.fi>
2020-02-02 16:55:27 +0200

    Use VirtualBox shared folder for Docker volume

--- build-elf.sh
+++ build-elf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
 PS2SDK_REVISION=e2afe92
 
 rm OPNPS2LD*.ELF
 docker build --tag pvalkone/opl:latest --build-arg PS2SDK_REVISION=${PS2SDK_REVISION} .
-docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c "git add --all && HIRES=1 make clean rebuild"
-OPLVERSION="$(docker run --volume "$PWD:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c 'git add --all && make oplversion')-HIRES"
+# VBoxManage sharedfolder add "default" --name "OPL" --hostpath "$(pwd)" --automount
+docker run --volume "/OPL:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c "git add --all && HIRES=1 make clean rebuild"
+OPLVERSION="$(docker run --volume "/OPL:/src" --rm --name opl pvalkone/opl:latest /bin/bash -c 'git add --all && make oplversion')-HIRES"
 mv OPNPS2LD.ELF "./OPNPS2LD-${OPLVERSION}.ELF"
 git reset
